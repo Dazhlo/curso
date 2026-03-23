06c8f81e80963418e6fcf268f22dfe63ccadb659
Dazhlo <dazhlo12@gmail.com>
2026-03-23 14:58:39 -0600

    links

--- components/navbar/Navbar..tsx
+++ components/navbar/Navbar..tsx
@@ -1,14 +1,16 @@
 
+import Link from "next/link";
 
-export const Navbar = () => {
+
+
+export const Navbar = async() => {
     return (
         <nav className="flex bg-blue-900 bg-opacity-30 p-2 m-2 rounded ">
-            <span>Home</span>
-
+     
             <div className="flex flex-1">
-                <a className="mr-2" href="/about">about</a>
-                <a className="mr-2" href="/contact">contect</a>
-                <a className="mr-2" href="/pricing">pricing</a>
+                <Link className="mr-2" href="/about">about</Link>
+                <Link className="mr-2" href="/contact">contect</Link>
+                <Link className="mr-2" href="/pricing">pricing</Link>
             </div>
         </nav>
 
